a8652920d903f284f078b65eba8f97da92642c2d
Julián Manrique <ja.manrique@uniandes.edu.co>
2018-07-16 14:51:37 -0500

    Component test

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,87 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <Schedule 
+			:time-ground="['09:00', '18:00']" 
+			:week-ground="['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']" 
+			:task-detail="[
+				[
+					{
+						dateStart: '09:30',
+						dateEnd: '10:30',
+						title: 'Metting',
+						detail: 'Metting (German: Mettingen) is a commune in the Moselle department in Grand Est in north-eastern France.',
+            detail
+					},
+					{
+						dateStart: '11:30',
+						dateEnd: '13:50',
+						title: 'Metting',
+						detail: 'Metting (German: Mettingen) is a commune in the Moselle department in Grand Est in north-eastern France.'
+					}
+					
+				],
+				[
+					{
+						dateStart: '10:30',
+						dateEnd: '12:00',
+						title: 'Metting',
+					},
+					{
+						dateStart: '12:30',
+						dateEnd: '14:50',
+						title: 'Metting',
+					}
+					
+				],
+				[
+					{
+						dateStart: '12:30',
+						dateEnd: '13:30',
+						title: 'Metting',
+					},
+					{
+						dateStart: '15:30',
+						dateEnd: '16:50',
+						title: 'Metting',
+					}
+					
+				],
+				[
+					{
+						dateStart: '09:50',
+						dateEnd: '10:50',
+						title: 'Metting',
+					},
+					{
+						dateStart: '11:30',
+						dateEnd: '13:50',
+						title: 'Metting',
+					}
+					
+				],
+				[
+					{
+						dateStart: '12:30',
+						dateEnd: '13:30',
+						title: 'Metting',
+					},
+					{
+						dateStart: '14:30',
+						dateEnd: '15:50',
+						title: 'Metting',
+					}
+					
+				]
+			]">
+				
+		</Schedule>
   </div>
 </template>
 
 <script>
+
+import courses from './../courses';
+
 export default {
   name: 'app',
   data () {
